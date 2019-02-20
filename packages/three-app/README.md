7452d935de5e9bdd21bdf98ea67cb174689b31d2
Olivier Lange <olange@petit-atelier.ch>
2019-02-20 19:17:02 +0100

    Handles resizing of ‹three-app› element (#2)

--- packages/three-app/README.md
+++ packages/three-app/README.md
@@ -6,4 +6,5 @@ Three.js app container, that provides animation timing and registry of canvases,
 
 1. Imports [THREE.js](https://github.com/mrdoob/three.js/) and makes it available in [Window global scope](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope);
 2. Handles _resizing_ of its bounding box — propagating the change to the aspect ratio to the cameras and renderers;
-3. Animates the scenes, cameras and objects — synchronized in the same animation frame, running at a desired frequency.
+3. Animates the scenes and cameras — synchronized in the same animation step, running at a desired FPS (if possible);
+4. Renders the current scene, at the same desired or actual FPS.
--- packages/three-app/three-app.js
+++ packages/three-app/three-app.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from "lit-element";
+import * as THREE from "three";
 
 const DEFAULTS = { fps: 60 };
 
@@ -6,15 +7,22 @@ export class ThreeApp extends LitElement {
 
   static get styles() {
     return css`
-      :host { display: block; }
-      :host([ hidden]) { display: none; }
+      :host { display: flex; flex-direction: column; position: relative }
+      :host([ hidden]) { display: none }
+      #display { height: 100% }
+      #info { position: absolute; top: 1em; right: 1em; color: white }
+      #info > * { margin: 0 0 1em }
     `;
   }
 
   render() {
     return html`
-      <h1>ThreeApp</h1>
-      <p>Desired ${this.fps} FPS (a frame about every ${this.interval} ms).</p>
+      <div id="info">
+        <p>Desired: ${this.fps} FPS (a frame about every ${this._interval} ms)<br/>
+          Actual: ${this._fpsActual} FPS (a frame about every ${this._intervalActual} ms)</p>
+      </div>
+      <canvas id="display"></canvas>
+      <slot></slot>
     `;
   }
 
@@ -23,7 +31,7 @@ export class ThreeApp extends LitElement {
    */
   static get properties() {
     return {
-      /** Target FPS */
+      /** Desired FPS */
       fps: { type: Number, reflect: true }
     };
   }
@@ -35,7 +43,7 @@ export class ThreeApp extends LitElement {
     const oldVal = this._fps;
     // newVal is set to `null` by Lit-Element, when attribute is removed
     this._fps = (newVal == null) ? DEFAULTS.fps : Math.floor( newVal);
-    this.interval = Math.floor( 1000 / this._fps); // ms
+    this._interval = Math.floor( 1000 / this._fps); // ms
     this.requestUpdate( "fps", oldVal);
   }
 
@@ -43,6 +51,8 @@ export class ThreeApp extends LitElement {
    * In the element constructor, assign default property values.
    */
   constructor() {
+    console.log( "three-app › constructor()");
+
     // Must call superconstructor first.
     super();
 
@@ -53,28 +63,119 @@ export class ThreeApp extends LitElement {
     this.resize = this.resizeCallback.bind( this);
 
     // Initialize internal properties
-    this._fps = undefined;
-    this.interval = undefined;
-    this.time = undefined;
-    this.lastTime = undefined;
+    this._initialized = false;
+
+    this._canvas = undefined;         // a reference to our ‹canvas› element
+    this._renderer = undefined;       // the THREE WebGL renderer used to draw to our canvas
+    this._displayRatio = undefined;   // current display ratio of the canvas, computed
+
+    this._fps = undefined;            // defined by `fps` property setter
+    this._fpsActual = undefined;      // computed by `this.tick()`
+    this._interval = undefined;       // derived from `this.fps`, computed by `fps` property setter
+    this._intervalActual = undefined; // computed by `this.tick()`
+    this._time = undefined;           // computed by `this.tick()`
+    this._lastTime = undefined;       // computed by `this.tick()`
+
     this.scenes = [];
     this.cameras = [];
-    this.canvases = [];
-    this.renderers = [];
 
-    // Initialize public properties
+    // Initialize public properties (must come after internal properties)
     this.fps = DEFAULTS.fps; // will trigger computation of this._interval
+  }
 
-    // Start the animation
+  /**
+   * Initializes the renderer and starts the animation loop.
+   *
+   * One-time call, which happens after the Shadow DOM was first
+   * rendered by Lit-Element. Ensures the ‹canvas› element is available.
+   */
+  firstUpdated() {
+    console.log( "three-app › firstUpdated()");
+
+    // Initializes the WebGL renderer and links it to our ‹canvas› element
+    this.init();
+
+    // Start the animation loop and timer
     this.start();
   }
 
   /**
-   * Implement firstUpdated to perform one-time work on first update:
-   * - Call a method to load the lazy element if necessary
-   * - Focus the checkbox
+   * Intializes the WebGL renderer and our canvas.
+   *
+   * Should be called from within `firstUpdated()`, once the Shadow DOM
+   * was rendered by Lit-Element.
    */
-  firstUpdated() {
+  init() {
+    console.log( "three-app › init()");
+
+    // Get and keep a reference to the our ‹canvas› element
+    this._canvas = this.shadowRoot.getElementById( "display");
+
+    // Instantiates a Three WebGL renderer, rendering to our ‹canvas› element
+    this._renderer = new THREE.WebGLRenderer(
+      { antialias: true, canvas: this._canvas });
+
+    // Update size of the display buffer of the renderer
+    this.resize();
+
+    // From now on, `start()` can be called to animate and render the scenes
+    this._initialized = true;
+  }
+
+  /**
+   * Starts the main animation loop and timer.
+   *
+   * Should be called from within `firstUpdated()`, once the Shadow DOM
+   * was rendered by Lit-Element, and only after `init()` was called.
+   */
+  start() {
+    console.assert( this._initialized, "three-app › start(): element incompletely initialized; call `init()` first.");
+    console.log( "three-app › start()");
+
+    this._lastTime = 0;
+    window.requestAnimationFrame( this.tick);
+  }
+
+  /**
+   * The main animation timer and loop. Called automatically once per
+   * browser frame, as a result of `window.requestAnimationFrame()`.
+   *
+   * Actions performed:
+   *
+   * 1. Updates the local tick value;
+   * 2. Updates and renders each scene in turn, at desired FPS, if possible;
+   * 3. Schedules another call to requestAnimationFrame.
+   *
+   * @param {number} time The current time; a high-resolution timer value,
+   *   as it comes from `window.requestAnimationFrame()`.
+   */
+  tickCallback( time) {
+    this._time = time;
+    const delta = time - this._lastTime;
+
+    if( delta >= this._interval) {
+      this.updateTimings( delta);
+      this.step( time, delta);
+      this._lastTime = time;
+    }
+
+    window.requestAnimationFrame( this.tick); // `this.tick()` is this `tickCallback()` bound to each instance of this class; see constructor
+  }
+
+  /**
+   * Updates the actual FPS and actual interval timing properties,
+   * computed from the current `delta` interval.
+   * @param {number} delta Time in millisecond, elapsed since last tick.
+   */
+  updateTimings( delta) {
+    const _intervalActualCurr = this._intervalActual;
+    if (delta !== _intervalActualCurr) {
+      const _fpsActualCurr = this._fpsActual;
+      this._intervalActual = delta;
+      this._fpsActual = Math.ceil(1000 / delta);
+      this.requestUpdate( "_intervalActual", _intervalActualCurr);
+      this.requestUpdate( "_fpsActual", _fpsActualCurr);
+    }
   }
 
   /**
@@ -89,7 +190,6 @@ export class ThreeApp extends LitElement {
    * @param {number} delta The delta time in ms since the last animation interval.
    */
   stepCallback( time, delta) {
-    // console.log( { time, delta, lastTime: this.lastTime, fps: this.fps });
     if( this.needsResize()) { this.resize(); }
     // this.scenes.update( time, delta);
     // this.cameras.update( time, delta);
@@ -97,49 +197,45 @@ export class ThreeApp extends LitElement {
   }
 
   /**
-   * The main animation timer. Called automatically once per browser frame,
-   * as a result of `window.requestAnimationFrame()`. Actions performed:
-   *
-   * 1. Updates the local tick value;
-   * 2. Updates and renders each scene in turn, if animation interval elapsed;
-   * 3. Schedules another call to requestAnimationFrame.
-   *
-   * @param {number} time The current time; a high-resolution timer value,
-   *   as it comes from `window.requestAnimationFrame()`.
+   * Returns the client width and height, as computed by the browser,
+   * and display ratio, of our canvas.
    */
-  tickCallback( time) {
-    this.time = time;
-    const delta = time - this.lastTime;
-
-    if( delta >= this.interval) {
-      this.step( time, delta);
-      this.lastTime = this.time;
-    }
-
-    window.requestAnimationFrame( this.tick); // `this.tick()` is the `tickCallback()` bound to each instance of this class; see constructor
+  getDisplaySize() {
+    const width  = this._canvas.clientWidth,
+          height = this._canvas.clientHeight,
+          ratio  = width / height;
+    return { width, height, ratio };
   }
 
-  start() {
-    this.lastTime = 0;
-    window.requestAnimationFrame( this.tick);
-  }
-
+  /**
+   * Returns true, when the size of the internal display buffer of
+   * the renderer does not match the actual client size of our canvas;
+   * false otherwise.
+   */
   needsResizeCallback() {
-    return false;
-    // const displayWidth = this._canvasElement.clientWidth;
-    // const displayHeight = this._canvasElement.clientHeight;
-    // const renderSize = this.renderer.getSize();
-    // return( renderSize.width != displayWidth || renderSize.height != displayHeight);
+    const { width, height } = this.getDisplaySize();
+    const renderSize = this._renderer.getSize();
+    return( renderSize.width !== width || renderSize.height !== height);
   }
 
+  /**
+   * Updates the size of the display buffer of the renderer,
+   * as well as the aspect ratio and the projection matrix of all cameras,
+   * to match the actual client size of our canvas.
+   */
   resizeCallback() {
-    // const displayWidth = this._canvasElement.clientWidth;
-    // const displayHeight = this._canvasElement.clientHeight;
-    // const displayRatio = displayWidth / displayHeight;
-    // console.log( `my-anim › resize() to ${displayWidth}x${displayHeight} (1:${displayRatio})`);
-    // this.camera.aspect = displayRatio;
-    // this.camera.updateProjectionMatrix();
-    // this.renderer.setSize( displayWidth, displayHeight, false);
+    const { width, height, ratio } = this.getDisplaySize();
+    console.log( `three-app › resize() to ${width}x${height}px (ratio of 1:${ratio})`);
+
+    // Update the aspect and projection matrix of all cameras,
+    // to match the new display ratio
+    this.cameras.forEach(( camera) => {
+      camera.aspect = ratio;
+      camera.updateProjectionMatrix();
+    });
+
+    // Update the renderer display buffer, to match the new display size
+    this._renderer.setSize( width, height, false);
   }
 }
 
