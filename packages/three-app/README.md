13ed3154bf253f327cb7fa43e40fa84c875663d6
Olivier Lange <olange@petit-atelier.ch>
2019-02-20 14:17:28 +0100

    Initial timing implementation for ‹three-app› element (#2)

--- packages/three-app/README.md
+++ packages/three-app/README.md
@@ -1,4 +1,9 @@
 # \<three-app\>
 
-Three app container, that provides animation timing and registry of renderers and scenes.
+Three.js app container, that provides animation timing and registry of canvases, renderers, cameras and scenes — allowing to synchronize animation and rendering of all its objects.
 
+## Features
+
+1. Imports [THREE.js](https://github.com/mrdoob/three.js/) and makes it available in [Window global scope](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope);
+2. Handles _resizing_ of its bounding box — propagating the change to the aspect ratio to the cameras and renderers;
+3. Animates the scenes, cameras and objects — synchronized in the same animation frame, running at a desired frequency.
--- packages/three-app/three-app.js
+++ packages/three-app/three-app.js
@@ -1,4 +1,3 @@
-// Import LitElement base class and html helper function
 import { LitElement, html, css } from "lit-element";
 
 export class ThreeApp extends LitElement {
@@ -13,14 +12,28 @@ export class ThreeApp extends LitElement {
   render() {
     return html`
       <h1>ThreeApp</h1>
-      <p>${this.message}</p>
+      <p>Desired ${this.fps} FPS (a frame about every ${this._interval} ms).</p>
     `;
   }
 
+  /**
+   * Observed properties, either reflected to attributes or not.
+   */
   static get properties() {
     return {
-      message: { type: String },
+      /** Target FPS */
+      fps: { type: Number, reflect: true }
     };
+  }
+
+  // Getter and setter for FPS property:
+  // computes derived internal `_interval` property
+  get fps() { return this._fps; }
+  set fps( newVal) {
+    const oldVal = this._fps;
+    this._fps = Math.floor( newVal); // FPS
+    this._interval = Math.floor( 1000 / this._fps); // ms
+    this.requestUpdate( "fps", oldVal);
   }
 
   /**
@@ -30,8 +43,26 @@ export class ThreeApp extends LitElement {
     // Must call superconstructor first.
     super();
 
-    // Initialize properties
-    this.message = "Hello World from LitElement";
+    // Bind callback methods to this instance
+    this.step = this.stepCallback.bind( this);
+    this.needsResize = this.needsResizeCallback.bind( this);
+    this.resize = this.resizeCallback.bind( this);
+
+    // Initialize internal properties
+    this._fps = undefined;
+    this._interval = undefined;
+    this.time = undefined;
+    this.lastTime = undefined;
+    this.scenes = [];
+    this.cameras = [];
+    this.canvases = [];
+    this.renderers = [];
+
+    // Initialize public properties
+    this.fps = 60; // triggers computation of this._interval
+
+    // Start the animation
+    this.start();
   }
 
   /**
@@ -40,6 +71,56 @@ export class ThreeApp extends LitElement {
    * - Focus the checkbox
    */
   firstUpdated() {
+  }
+
+  /**
+   * The main animation step. Called automatically once per browser frame,
+   * as a result of `window.requestAnimationFrame()`. Actions performed:
+   *
+   * 1. Updates the local tick value;
+   * 2. Updates each scene in turn;
+   * 3. Renders each scene in turn;
+   * 3. Schedules another call to requestAnimationFrame.
+   *
+   * @param {number} time The current time; a high-resolution timer value, as it comes from `window.performance.now()`.
+   * @param {number} delta The delta time in ms since the last frame; a smoothed and capped value based on the FPS rate.
+   */
+  stepCallback( ignoredTime) {
+    this.time = window.performance.now();
+    const delta = this.time - this.lastTime;
+
+    if( delta >= this._interval) {
+      if( this.needsResize()) { this.resize(); }
+      // this.scenes.update( time, delta);
+      // this.cameras.update( time, delta);
+      // this.renderers.render();
+      this.lastTime = this.time;
+    }
+
+    window.requestAnimationFrame( this.step); // `this.step()` is the `stepCallback()` bound to each instance of this class; see constructor
+  }
+
+  start() {
+    this.lastTime = 0;
+    window.requestAnimationFrame( this.step);
+  }
+
+  needsResizeCallback() {
+    return false;
+    // const displayWidth = this._canvasElement.clientWidth;
+    // const displayHeight = this._canvasElement.clientHeight;
+    // const renderSize = this.renderer.getSize();
+    // return( renderSize.width != displayWidth || renderSize.height != displayHeight);
+  }
+
+  resizeCallback() {
+    // const displayWidth = this._canvasElement.clientWidth;
+    // const displayHeight = this._canvasElement.clientHeight;
+    // const displayRatio = displayWidth / displayHeight;
+    // console.log( `my-anim › resize() to ${displayWidth}x${displayHeight} (1:${displayRatio})`);
+    // this.camera.aspect = displayRatio;
+    // this.camera.updateProjectionMatrix();
+    // this.renderer.setSize( displayWidth, displayHeight, false);
   }
 }
 
